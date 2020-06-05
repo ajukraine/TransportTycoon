8e24c7cba5a0a5f9b542820986ca65f7b537c17b
Bohdan Makohin <ajukraine@gmail.com>
2020-06-06 02:45:26 +0300

    Improve solution

--- TransportTycoon.ConsoleApp/Solution.cs
+++ TransportTycoon.ConsoleApp/Solution.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace TransportTycoon.ConsoleApp
 {
@@ -22,137 +21,135 @@ namespace TransportTycoon.ConsoleApp
 
             var vehicles = new Vehicle[]
             {
-                new Vehicle("Truck 1", "Factory"),
-                new Vehicle("Truck 2", "Factory"),
-                new Vehicle("Ship", "Port")
+                new Vehicle(VehicleType.Truck, "Factory"),
+                new Vehicle(VehicleType.Truck, "Factory"),
+                new Vehicle(VehicleType.Ship, "Port")
             };
-            
+
             IEnumerable<Order> MakeOrders(Vehicle vehicle)
             {
-                if (!vehicle.CanTakeOrders()) yield break;
+                if (!vehicle.CanAssignOrders()) yield break;
 
-                if (vehicle.Location == "Factory" && vehicle.Name != "Ship")
+                if (vehicle.Location == "Factory" && vehicle.Type == VehicleType.Truck)
                 {
                     if (!factory.TryDequeue(out var cargo)) yield break;
 
                     if (cargo == 'A')
                     {
-                        yield return new Order(map["PORT"], () => port.Enqueue('A'));
-                        yield return new Order(map["PORT"], true);
+                        yield return new Order(map["PORT"], () => port.Enqueue(cargo));
+                        yield return new Order(map["PORT"].Backward());
                     }
                     if (cargo == 'B')
                     {
-                        yield return new Order(map["B"], () => warehouseB.Enqueue('B'));
-                        yield return new Order(map["B"], true);
+                        yield return new Order(map["B"], () => warehouseB.Enqueue(cargo));
+                        yield return new Order(map["B"].Backward());
                     }
                 }
 
-                if (vehicle.Location == "Port" && vehicle.Name == "Ship")
+                if (vehicle.Location == "Port" && vehicle.Type == VehicleType.Ship)
                 {
                     if (!port.TryDequeue(out var cargo)) yield break;
                     
                     if (cargo == 'A')
                     {
-                        yield return new Order(map["A"], () => warehouseA.Enqueue('A'));
-                        yield return new Order(map["A"], true);
+                        yield return new Order(map["A"], () => warehouseA.Enqueue(cargo));
+                        yield return new Order(map["A"].Backward());
                     }
                 }
             }
 
-            var time = 0;
+            var time = new Time();
 
             int numberOfContainers = factory.Count;
             bool AllContainersAreDelivered() =>
                 numberOfContainers == warehouseA.Count + warehouseB.Count;
 
-            while (!AllContainersAreDelivered() && time != -1)
+            while (!AllContainersAreDelivered() && !time.HasExpired())
             {
                 foreach(var vehicle in vehicles)
-                {
                     vehicle.AssignOrders(MakeOrders(vehicle));
-                }
 
                 foreach(var vehicle in vehicles)
-                {
-                    vehicle.ForwardTime();
-                }
+                    vehicle.Move();
 
-                if (++time > 1000) time = -1;
+                time.NextTick();
             }
 
-            return time;
+            return time.CurrentTime;
         }   
+    }
+
+    class Time
+    {
+        public int CurrentTime { get; private set; }
+        private int _expirationTime;
+
+        public Time(int expirationTime = 1000) => _expirationTime = expirationTime;
+
+        public void NextTick() => ++CurrentTime;
+
+        public bool HasExpired() => CurrentTime >= _expirationTime;
     }
 
     class Track
     {
-        public string LocationA { get; set; }
-
-        public string LocationB { get; set; }
-
+        public string StartLocation { get; set; }
+        public string FinishLocation { get; set; }
         public int TravelTime { get; set; }
 
-        public Track(string pointA, string pointB, int travelTime) =>
-            (LocationA, LocationB, TravelTime) = (pointA, pointB, travelTime);
+        public Track(string startLocation, string finishLocation, int travelTime) =>
+            (StartLocation, FinishLocation, TravelTime) = (startLocation, finishLocation, travelTime);
+        
+        private Track _backwardTrack;
+        public Track Backward() =>
+            (_backwardTrack ??= new Track(FinishLocation, StartLocation, TravelTime));
     }
 
     class Order
     {
-        public static readonly string InProgress = "IN_PROGRESS";
+        public static readonly string IN_PROGRESS = "IN_PROGRESS";
 
-        private int _timeOnTrack;
-
+        private int _timeSpentOnTrack;
         private Track _track;
-
-        private bool _isBackward;
-
         private Action _finishCallback;
 
-        public Order(Track track, bool isBackward = false) =>
-            (_track, _isBackward, _timeOnTrack) = (track, isBackward, 0);
+        public Order(Track track, Action finishCallback = null) =>
+            (_track, _timeSpentOnTrack, _finishCallback) = (track, 0, finishCallback);
 
-        public Order(Track track, Action finishCallback) : this(track)
-        {
-            _finishCallback = finishCallback;
-        }
+        public bool IsFinished() => _track.TravelTime == _timeSpentOnTrack;
 
-        public bool IsFinished() => _track.TravelTime == _timeOnTrack;
-
-        public bool HasStarted() => _timeOnTrack != 0;
+        public bool HasStarted() => _timeSpentOnTrack != 0;
 
         public string GetCurrentLocation()
         {
-            if (!HasStarted()) return _isBackward ? _track.LocationB : _track.LocationA;
-            if (IsFinished()) return _isBackward ? _track.LocationA : _track.LocationB;
+            if (!HasStarted()) return _track.StartLocation;
+            if (IsFinished()) return _track.FinishLocation;
 
-            return InProgress;
+            return IN_PROGRESS;
         }
 
-        public void ForwardTime()
+        public void Proceed()
         {
-            ++_timeOnTrack;
+            ++_timeSpentOnTrack;
 
             if (IsFinished())
                 _finishCallback?.Invoke();
         }
     }
 
+    public enum VehicleType { Truck, Ship }
+
     class Vehicle
     {
         private Queue<Order> _orders;
-
         private Order _currentOrder;
+
+        public VehicleType Type { get; set; }
 
         public string Location { get; set; }
 
-        public string Name { get; set; }
-
-        public Vehicle(string name, string location)
-        {
-            (Name, Location) = (name, location);
-
-            _orders = new Queue<Order>();
-        }
+        public Vehicle(VehicleType type, string location) =>
+            (Location, Type, _orders) = (location, type, new Queue<Order>());
 
         public void AssignOrders(IEnumerable<Order> orders)
         {
@@ -160,17 +157,15 @@ namespace TransportTycoon.ConsoleApp
                 _orders.Enqueue(order);
         }
 
-        public bool CanTakeOrders() => _orders.Count == 0 && (_currentOrder?.IsFinished() ?? true);
+        public bool CanAssignOrders() => _orders.Count == 0 && (_currentOrder?.IsFinished() ?? true);
 
-        public void ForwardTime()
+        public void Move()
         {
             if (_currentOrder == null || _currentOrder.IsFinished())
-            {
-                var tookNewOrder = _orders.TryDequeue(out _currentOrder);
-                if (!tookNewOrder) return;
-            }
+                if (!_orders.TryDequeue(out _currentOrder))
+                    return;
 
-            _currentOrder.ForwardTime();
+            _currentOrder.Proceed();
             Location = _currentOrder.GetCurrentLocation();
         }
     }
