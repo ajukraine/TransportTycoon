9b22b581affe871cca16cd2fb7a06d07b6f7a6fd
Bohdan Makohin <ajukraine@gmail.com>
2020-06-06 19:47:13 +0300

    Refactor solution structure to support code coverage

--- Exercise-1/TransportTycoon.ConsoleApp/Program.cs
+++ Exercise-1/TransportTycoon.ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using TransportTycoon.Domain;
 
 namespace TransportTycoon.ConsoleApp
 {
--- Exercise-1/TransportTycoon.Tests/SolutionTests.cs
+++ Exercise-1/TransportTycoon.Tests/SolutionTests.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-using TransportTycoon.ConsoleApp;
+using TransportTycoon.Domain;
 
 namespace TransportTycoon.Tests
 {
